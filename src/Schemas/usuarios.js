8c474d21a97271fd43828c28464d4ed9b165887c
Bruno Pilla <brunopilla@hotmail.com>
2023-04-21 22:41:40 -0300

    Melhorando validação joi

--- src/Schemas/usuarios.js
+++ src/Schemas/usuarios.js
@@ -4,18 +4,21 @@ const schemaUsuario = joi.object({
     nome: joi.string().max(100).required().messages({
         'string.base': 'O campo nome precisa ser do tipo alfabético',
         'string.max': 'Não exceder 100 caracteres para o campo nome',
-        'any.required': 'O campo nome é obrigatório'
+        'any.required': 'O campo nome é obrigatório',
+        'string.empty': 'O campo nome é obrigatório'
     }),
     email: joi.string().max(100).email().required().messages({
         'string.base': 'O campo e-mail precisa ser do tipo alfabético',
         'string.max': 'Não exceder 100 caracteres para o campo email',
         'string.email': 'E-mail inválido',
-        'any.required': 'O campo email é obrigatório'
+        'any.required': 'O campo email é obrigatório',
+        'string.empty': 'O campo email é obrigatório'
     }),
     senha: joi.string().max(150).required().messages({
         'string.base': 'O campo senha precisa ser do tipo alfanumérico',
         'string.max': 'Não exceder 150 caracteres para o campo senha',
-        'any.required': 'O campo senha é obrigatório'
+        'any.required': 'O campo senha é obrigatório',
+        'sting.empty': 'O campo senha é obrigatório'
     })
 })
 
